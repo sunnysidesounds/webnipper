4ddcc2ece864a466b12003fd3d001454419cfaa3
Jason R Alexander <sunnysidesounds@gmail.com>
2015-02-12 22:31:21 -0800

    Changes to crawler main

--- src/com/webnipper/Webnipper.java
+++ src/com/webnipper/Webnipper.java
@@ -1,53 +1,78 @@
 package com.webnipper;
 
-import java.io.IOException;
 import java.util.StringTokenizer;
-
-import javax.xml.parsers.ParserConfigurationException;
-
 import org.apache.hadoop.util.ToolRunner;
 import org.apache.nutch.crawl.Crawl;
 import org.apache.nutch.indexer.solr.SolrIndexer;
-import org.apache.nutch.segment.SegmentReader;
 import org.apache.nutch.util.NutchConfiguration;
 import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.SolrServerException;
-import org.apache.solr.client.solrj.embedded.EmbeddedSolrServer;
 import org.apache.solr.client.solrj.response.QueryResponse;
 import org.apache.solr.common.SolrDocumentList;
-import org.apache.solr.core.CoreContainer;
-import org.xml.sax.SAXException;
+import org.apache.solr.client.solrj.impl.CommonsHttpSolrServer;
+
 
 public class Webnipper {
 
     public static void main(String[] args) {
 
-                /*
-                 * Arguments for crawling.
-                 *
-                 * -dir dir names the directory to put the crawl in.
-                 *
-                 * -threads threads determines the number of threads that will fetch in
-                 * parallel.
-                 *
-                 * -depth depth indicates the link depth from the root page that should
-                 * be crawled.
-                 *
-                 * -topN N determines the maximum number of pages that will be retrieved
-                 * at each level up to the depth.
-                 */
 
-        String crawlArg = "urls -dir crawl -threads 5 -depth 3 -topN 20";
-
-        // Run Crawl tool
-
+        //Run Crawl Tool
         try {
+            String crawlArg = "urls -dir crawl -threads 5 -depth 3 -topN 20";
+            System.out.println("Running Crawl Tool!");
+            System.out.println("**********************************");
             ToolRunner.run(NutchConfiguration.create(), new Crawl(),
                     tokenize(crawlArg));
         } catch (Exception e) {
             e.printStackTrace();
             return;
         }
+
+
+        //Run Solr Index Tool
+        try {
+            String indexArg = "http://localhost:8983/solr -reindex";
+            System.out.println("Running Solr Index Tool!");
+            System.out.println("**********************************");
+            ToolRunner.run(NutchConfiguration.create(), new SolrIndexer(),
+                    tokenize(indexArg));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
+
+        // Run Solr Query Tool
+        try {
+            String url = "http://localhost:8983/solr";
+            System.out.println("Running Solr Query Tool!");
+            System.out.println("**********************************");
+            CommonsHttpSolrServer server = new CommonsHttpSolrServer(url);
+            SolrQuery query = new SolrQuery();
+            query.setQuery("content:mycontent"); // Searching mycontent in query
+            query.addSortField("content", SolrQuery.ORDER.asc);
+            QueryResponse rsp;
+            try {
+                rsp = server.query(query);
+            } catch (SolrServerException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                return;
+            }
+            // Display the results in the console
+            SolrDocumentList docs = rsp.getResults();
+            for (int i = 0; i < docs.size(); i++) {
+                System.out.println(docs.get(i).get("title").toString() + " Link: "
+                        + docs.get(i).get("url").toString());
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+            return;
+        }
+
+         /*
+
+        // Old Code Keeping just or now.
 
         // Let's dump the segments we have to see what we have obtained. You
         // need to refresh your workspace to see the new folders. You can see
@@ -121,14 +146,15 @@ public class Webnipper {
         // Shut down the container so JVM ends.
         coreContainer.shutdown();
 
+        */
+
     }
 
     /**
      * Helper function to convert a string into an array of strings by
      * separating them using whitespace.
      *
-     * @param str
-     *            string to be tokenized
+     * @param str string to be tokenized
      * @return an array of strings that contain a each word each
      */
     public static String[] tokenize(String str) {
