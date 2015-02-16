0f935d22886115bbfd86c69521479365895cc82d
Jason R Alexander <sunnysidesounds@gmail.com>
2015-02-15 23:16:04 -0800

    Updates to regex

--- src/com/webnipper/Webnipper.java
+++ src/com/webnipper/Webnipper.java
@@ -29,7 +29,6 @@ public class Webnipper {
             return;
         }
 
-
         //Run Solr Index Tool
         try {
             String indexArg = "http://localhost:8983/solr -reindex";
@@ -70,83 +69,6 @@ public class Webnipper {
             return;
         }
 
-         /*
-
-        // Old Code Keeping just or now.
-
-        // Let's dump the segments we have to see what we have obtained. You
-        // need to refresh your workspace to see the new folders. You can see
-        // plaintext by going into dump folder and examining "dump".
-        String dumpArg = "-dump crawl/segments/* dump -nocontent -nofetch -nogenerate -noparse -noparsedata";
-
-        // Run dump
-        try {
-            SegmentReader.main(tokenize(dumpArg));
-        } catch (Exception e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-            return;
-        }
-
-        System.setProperty("solr.solr.home", "/home/emre/solr");
-        CoreContainer.Initializer initializer = new CoreContainer.Initializer();
-        CoreContainer coreContainer;
-
-        try {
-            coreContainer = initializer.initialize();
-
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-            return;
-        } catch (ParserConfigurationException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-            return;
-        } catch (SAXException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-            return;
-        }
-        EmbeddedSolrServer server = new EmbeddedSolrServer(coreContainer, "");
-
-        // Arguments for indexing
-        String indexArg = "local crawl/crawldb -linkdb crawl/linkdb crawl/segments/*";
-
-        // Run indexing tool
-        try {
-            ToolRunner.run(NutchConfiguration.create(),
-                    new SolrIndexer(server), tokenize(indexArg));
-        } catch (Exception e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-            return;
-        }
-
-        // Let's query for something!
-        SolrQuery query = new SolrQuery();
-        query.setQuery("title:queen"); // Searching queen in query
-        query.addSortField("title", SolrQuery.ORDER.asc);
-        QueryResponse rsp;
-        try {
-            rsp = server.query(query);
-        } catch (SolrServerException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-            return;
-        }
-
-        // Display the results in the console
-        SolrDocumentList docs = rsp.getResults();
-        for (int i = 0; i < docs.size(); i++) {
-            System.out.println(docs.get(i).get("title").toString() + " Link: "
-                    + docs.get(i).get("url").toString());
-        }
-
-        // Shut down the container so JVM ends.
-        coreContainer.shutdown();
-
-        */
 
     }
 
